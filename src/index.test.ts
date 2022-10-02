0348641be587986df74e731cb84d218efb7bceaa
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-10-01 20:22:06 -0700

    Add data prep functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,21 @@
-test('dummy test', () => {
-  expect(true).toBeTruthy()
+import { appendHeadersToData, CSVData, parseRawData, sortByTime } from './index'
+
+test('add headers to raw data', () => {
+  expect(appendHeadersToData(['id', 'time'], '48\t1653746491')).toEqual('id\ttime\n48\t1653746491')
+})
+
+test('parse raw data into data structure', () => {
+  expect(parseRawData('id\ttime\n48\t16')).toEqual([{ id: '48', time: '16' }])
+})
+
+test('sort entries by time', () => {
+  const unsortedEntries: CSVData[] = [
+    { id: '1', time: '5' },
+    { id: '2', time: '2' }
+  ]
+
+  expect(sortByTime(unsortedEntries)).toEqual([
+    { id: '2', time: '2' },
+    { id: '1', time: '5' }
+  ])
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export const hello = (name: string): string => 'Hello, ' + name + '!'
+import { parse } from 'papaparse'
+
+export const appendHeadersToData = (headers: string[], rawData: string): string => {
+  return headers.join('\t') + '\n' + rawData
+}
+
+export interface CSVData {
+  id: string
+  time: string
+}
+
+export const parseRawData = (rawData: string): CSVData[] => {
+  const { data } = parse<CSVData>(rawData, { header: true })
+
+  return data
+}
+
+export const sortByTime = (data: CSVData[]): CSVData[] => {
+  return data.sort((a, b) => Number(a.time) - Number(b.time))
+}
+
+export const index = (rawData: string): void => {
+  const dataWithHeaders = appendHeadersToData(['id', 'time'], rawData)
+  const parsedData = parseRawData(dataWithHeaders)
+  sortByTime(parsedData)
+}
