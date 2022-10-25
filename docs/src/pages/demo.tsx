ee6856c5d160b8e09aefd3aeebaf829281604648
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-10-25 09:51:32 -0700

    docs: style demo page

--- docs/src/pages/demo.tsx
+++ docs/src/pages/demo.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import ReactJson from "react-json-view";
+import ReactJson, { ThemeKeys } from "react-json-view";
 import { slalomX } from "slalomx";
 import { useMockData } from "@site/src/_utils/generator/useMockData";
 import { NewDataButton } from "@site/src/components/Demo/NewDataButton";
+import { useColorMode } from "@docusaurus/theme-common";
+import styles from "./demo.module.css";
 
-export default function Hello() {
+export default function Demo() {
   const { data, createNewDataSet } = useMockData(2);
 
   const config = {
@@ -17,15 +19,16 @@ export default function Hello() {
 
   return (
     <Layout title="Hello" description="Hello React Page">
-      <NewDataButton onClick={createNewDataSet} />
-      <JSONDataRow title={"Input"}>
+      <Header createNewDataSet={createNewDataSet} />
+
+      <JSONDataRow title={"Parameters"}>
         <JSONDataColumn name={"names"} src={data.names} />
         <JSONDataColumn name={"starts"} src={data.starts} />
         <JSONDataColumn name={"ends"} src={data.ends} />
         <JSONDataColumn name={"config"} src={config} />
       </JSONDataRow>
 
-      <JSONDataRow title={"Output"}>
+      <JSONDataRow title={"Return Value"}>
         <JSONDataColumn name={"ordered"} src={results.ordered} />
         <JSONDataColumn name={"personal"} src={results.personal} />
         <JSONDataColumn name={"overall"} src={results.overall} />
@@ -34,10 +37,36 @@ export default function Hello() {
   );
 }
 
+const Header = (props: { createNewDataSet: () => void }) => {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>Simulate a Race</h1>
+      <div className={styles.subtitle}>
+        Use this tool to create mock race data, and see how{" "}
+        <strong>Lap Up</strong> creates a race roster.
+      </div>
+      <NewDataButton onClick={props.createNewDataSet} />
+    </div>
+  );
+};
+
 const JSONDataColumn = (props: { name: string; src: any }) => {
+  const { colorMode } = useColorMode();
+
+  const colorDependentProps: { theme: ThemeKeys; borderColor: string } =
+    colorMode === "dark"
+      ? {
+          theme: "railscasts",
+          borderColor: "#c54e4e",
+        }
+      : {
+          theme: "rjv-default",
+          borderColor: "#5c7abb",
+        };
+
   return (
     <div style={{ flexGrow: 1, margin: 10 }}>
-      <h2>{props.name}</h2>
+      <h3>{props.name}</h3>
       <ReactJson
         name={props.name}
         src={props.src}
@@ -46,8 +75,12 @@ const JSONDataColumn = (props: { name: string; src: any }) => {
         displayDataTypes={false}
         // @ts-ignore
         displayArrayKey={false}
-        theme={"ocean"}
-        style={{ padding: 10 }}
+        theme={colorDependentProps.theme}
+        style={{
+          padding: 10,
+          border: `3px solid`,
+          borderColor: colorDependentProps.borderColor,
+        }}
         collapsed={2}
       />
     </div>
@@ -56,9 +89,9 @@ const JSONDataColumn = (props: { name: string; src: any }) => {
 
 const JSONDataRow = (props: { children: React.ReactNode; title: string }) => {
   return (
-    <div>
+    <div className={styles.row}>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1>{props.title}</h1>
+        <h2>{props.title}</h2>
       </div>
       <div style={{ display: "flex", padding: 10 }}>{props.children}</div>
     </div>
